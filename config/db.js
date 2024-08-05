d34f8c4a77ddb0f7913855fb8386ce55b282a2f3
chintandavda <davdachintan97@gmail.com>
2024-08-05 22:51:01 +0530

    docker file and mongo connection online

--- config/db.js
+++ config/db.js
@@ -3,11 +3,24 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB', error);
+//     process.exit(1);
+//   }
+// };
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      tls: true,
+      tlsAllowInvalidCertificates: true, // Try setting this to true if you encounter SSL issues
     });
     console.log('MongoDB connected');
   } catch (error) {
